65daa70ab8fdf3f571b39095fa314f8dac9caf49
JuManjey <opghld@gmail.com>
2022-05-14 16:12:18 +0300

    Initial

--- m8/epam.py
+++ m8/epam.py
@@ -2,10 +2,10 @@ from asyncio.windows_events import NULL
 from sqlite3 import Date
 import mymodule
 
-#mymodule.date_time()
-#mymodule.numbers()
-#mymodule.read_test_file()
-#mymodule.read_html_title()
-#mymodule.emoji_converter()
-#mymodule.strip_emoji()   
+mymodule.date_time()
+mymodule.numbers()
+mymodule.read_test_file()
+mymodule.read_html_title()
+mymodule.emoji_converter()
+mymodule.strip_emoji()   
 mymodule.emoji_converter2()
