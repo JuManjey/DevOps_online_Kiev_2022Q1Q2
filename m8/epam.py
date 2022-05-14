5ec5d9d5c1538bf2f281e7081154ce6aedd2d751
JuManjey <opghld@gmail.com>
2022-05-14 14:17:56 +0300

    Initial

--- m8/epam.py
+++ m8/epam.py
@@ -2,10 +2,10 @@ from asyncio.windows_events import NULL
 from sqlite3 import Date
 import mymodule
 
-mymodule.date_time()
-mymodule.numbers()
-
-
+#mymodule.date_time()
+#mymodule.numbers()
+#mymodule.read_test_file()
+mymodule.read_html_title()
 
         
 
--- m8/mymodule.py
+++ m8/mymodule.py
@@ -1,3 +1,4 @@
+from cgi import print_arguments
 from datetime import date
 from datetime import datetime
 
@@ -15,5 +16,39 @@ def numbers():
     print("List of numbers - " + ((str)(list_of_numbers)))
     tuple_of_numbers=(list_of_numbers);
     print("Tuple of numbers -" + ((str)(tuple_of_numbers)))
+    
+    
+def read_test_file():
+    name_of_file = input("Enter name of file:");
+    file = open(name_of_file, 'w')
+    file.write("This is some text");
+    file.close();
+    file = open('test_file.txt', 'r');
+    buf = file.read();
+    print(buf);
+    file.close()
+    
+    
+def read_html_title():
+    
+    file = open('index.html','r')
+    massive = []
+    substr = "title"
+    while True:
+        buf = file.readline()
+        if not buf:
+            break
+        if substr in buf.strip():
+            massive.append(buf.strip())
+    file.close()
+    for x in massive:
+        print(x)
+    
+        
+        
+        
+        
+    
+    
     
     
